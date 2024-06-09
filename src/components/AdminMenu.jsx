7292c4ed31ecde41d8bdacc464b70f82ade81ba1
Naba-skcn <saymakhorshed01@gmail.com>
2024-06-10 00:49:31 +0600

    menuitem added

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -1,12 +1,16 @@
 import { FaUserCog } from 'react-icons/fa'
+import { TbHomeCog } from "react-icons/tb";
+import { MdOutlineReviews } from "react-icons/md";
+import { RiAdvertisementFill } from "react-icons/ri";
 import MenuItem from './MenuItem'
 
 const AdminMenu = () => {
   return (
     <>
-      <MenuItem icon={FaUserCog} label='Manage Properties' address='/dashboard/manage-properties'></MenuItem>
+      <MenuItem icon={TbHomeCog} label='Manage Properties' address='/dashboard/manage-properties'></MenuItem>
+      <MenuItem icon={RiAdvertisementFill} label='Advertise property' address='/dashboard/advertise-properties'></MenuItem>
       <MenuItem icon={FaUserCog} label='Manage Users' address='/dashboard/manage-users' />
-      <MenuItem icon={FaUserCog} label='Manage Reviews' address='/dashboard/manage-reviews' />
+      <MenuItem icon={MdOutlineReviews} label='Manage Reviews' address='/dashboard/manage-reviews' />
 
     </>
   )
